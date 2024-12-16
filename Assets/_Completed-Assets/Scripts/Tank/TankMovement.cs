8c0d7a65aef31ec9aa103c03b1406848abbc850c
Nyuta-222 <Amayori22@outlook.jp>
2024-12-16 18:13:03 +0900

    砲塔の制御の実装

--- Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
@@ -10,7 +10,7 @@ namespace Complete
         public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
         public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
         public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
-		public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
+        public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
 
         private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
         private string m_TurnAxisName;              // The name of the input axis for turning.
@@ -20,13 +20,26 @@ namespace Complete
         private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
         private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
 
-        private void Awake ()
+        // 追加した変数
+        public float t_TurnInputValue; // 砲塔を回転するキーの入力量
+        public float t_TurnSpeed = 100f; // 砲塔の回転速度
+        private string t_TurnAxisName; // 手順1で定義した砲塔を回転するキーのName
+        public GameObject turrent; // 砲塔のゲームオブジェクトの参照
+
+
+        private void Awake()
         {
-            m_Rigidbody = GetComponent<Rigidbody> ();
+            m_Rigidbody = GetComponent<Rigidbody>();
+
+            // 砲台のnullチェック
+            if (turrent == null)
+            {
+                Debug.LogError("砲台が正しく参照されていません。");
+            }
         }
 
 
-        private void OnEnable ()
+        private void OnEnable()
         {
             // When the tank is turned on, make sure it's not kinematic.
             m_Rigidbody.isKinematic = false;
@@ -34,6 +47,7 @@ namespace Complete
             // Also reset the input values.
             m_MovementInputValue = 0f;
             m_TurnInputValue = 0f;
+            t_TurnInputValue = 0f;
 
             // We grab all the Particle systems child of that Tank to be able to Stop/Play them on Deactivate/Activate
             // It is needed because we move the Tank when spawning it, and if the Particle System is playing while we do that
@@ -46,52 +60,58 @@ namespace Complete
         }
 
 
-        private void OnDisable ()
+        private void OnDisable()
         {
             // When the tank is turned off, set it to kinematic so it stops moving.
             m_Rigidbody.isKinematic = true;
 
             // Stop all particle system so it "reset" it's position to the actual one instead of thinking we moved when spawning
-            for(int i = 0; i < m_particleSystems.Length; ++i)
+            for (int i = 0; i < m_particleSystems.Length; ++i)
             {
                 m_particleSystems[i].Stop();
             }
         }
 
 
-        private void Start ()
+        private void Start()
         {
             // The axes names are based on player number.
             m_MovementAxisName = "Vertical" + m_PlayerNumber;
             m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+
+            // 砲台を回転するキーのNameを定義
+            t_TurnAxisName = "TurretTurn" + m_PlayerNumber;
 
             // Store the original pitch of the audio source.
             m_OriginalPitch = m_MovementAudio.pitch;
         }
 
 
-        private void Update ()
+        private void Update()
         {
             // Store the value of both input axes.
-            m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
-            m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
+            m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+            m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
 
-            EngineAudio ();
+            // 塔の回転キーの入力
+            t_TurnInputValue = Input.GetAxis(t_TurnAxisName);
+
+            EngineAudio();
         }
 
 
-        private void EngineAudio ()
+        private void EngineAudio()
         {
             // If there is no input (the tank is stationary)...
-            if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
+            if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
             {
                 // ... and if the audio source is currently playing the driving clip...
                 if (m_MovementAudio.clip == m_EngineDriving)
                 {
                     // ... change the clip to idling and play it.
                     m_MovementAudio.clip = m_EngineIdling;
-                    m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                    m_MovementAudio.Play ();
+                    m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                    m_MovementAudio.Play();
                 }
             }
             else
@@ -108,15 +128,18 @@ namespace Complete
         }
 
 
-        private void FixedUpdate ()
+        private void FixedUpdate()
         {
             // Adjust the rigidbodies position and orientation in FixedUpdate.
-            Move ();
-            Turn ();
+            Move();
+            Turn();
+
+            // 砲台の制御
+            TurretTurn();
         }
 
 
-        private void Move ()
+        private void Move()
         {
             // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
             Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
@@ -126,16 +149,33 @@ namespace Complete
         }
 
 
-        private void Turn ()
+        private void Turn()
         {
             // Determine the number of degrees to be turned based on the input, speed and time between frames.
             float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
 
             // Make this into a rotation in the y axis.
-            Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
 
             // Apply this rotation to the rigidbody's rotation.
-            m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+            m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+        }
+
+        /// <summary>
+        /// 砲台の制御
+        /// </summary>
+        private void TurretTurn()
+        {
+            // t_TurnInputValueの入力値・回転速度・時間に基づいて砲台の回転角を計算
+            float turretRotation = t_TurnInputValue * t_TurnSpeed * Time.deltaTime;
+
+            // 現在の回転を取得
+            Quaternion currentrotation = turrent.transform.rotation;
+            // y軸の回転の作成（y軸のみ設定）
+            Quaternion turnRotation = Quaternion.Euler(0f, turretRotation, 0f);
+
+            // y軸更新（現在の砲台の向きにy軸の回転を反映）
+            turrent.transform.rotation = currentrotation * turnRotation;
         }
     }
 }
